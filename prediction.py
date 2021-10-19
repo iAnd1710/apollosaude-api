74d1e9b8287195d0c76b57d3c3211c87b2689f36
gams <gustavomaedoyt@gmail.com>
2021-10-19 19:54:36 -0300

    Pikkaaa

--- prediction.py
+++ prediction.py
@@ -3,6 +3,14 @@ import numpy as np
 from scipy.stats import mode
 from sklearn.preprocessing import LabelEncoder
 import pickle
+import json
+
+# Opening JSON file
+dpt = open('./datasets/pt.json', )
+
+# returns JSON object as
+# a dictionary
+ptdata = json.load(dpt)
 
 final_rf_model = pickle.load(open('rf_model.pkl', 'rb'))
 final_nb_model = pickle.load(open('rf_model.pkl', 'rb'))
@@ -49,10 +57,17 @@ def predict_disease(symptoms):
 
     final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
 
-    dataDesc = pd.read_csv("./datasets/symptom_description.csv", encoding="ISO-8859-1")
-    description = dataDesc.loc[dataDesc['Disease'] == final_prediction, 'Description'].iloc[0]
+    dataPrec = pd.read_csv("./datasets/symptom_precaution.csv", encoding="ISO-8859-1")
+    prec1 = dataPrec.loc[dataPrec['Disease'] == final_prediction, 'Precaution_1'].iloc[0]
+    prec1 = prec1[:1].upper() + prec1[1:]
+    prec2 = dataPrec.loc[dataPrec['Disease'] == final_prediction, 'Precaution_2'].iloc[0]
+    prec2 = prec2[:1].upper() + prec2[1:]
+    prec3 = dataPrec.loc[dataPrec['Disease'] == final_prediction, 'Precaution_3'].iloc[0]
+    prec3 = prec3[:1].upper() + prec3[1:]
+    prec4 = dataPrec.loc[dataPrec['Disease'] == final_prediction, 'Precaution_4'].iloc[0]
+    prec4 = prec4[:1].upper() + prec4[1:]
     predictions = {
         "disease": final_prediction,
-        "description": description
+        "description": '%s. %s. %s. %s.' % (prec1, prec2, prec3, prec4)
     }
     return predictions
