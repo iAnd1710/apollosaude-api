62750bbdc15d140940239d7e1e4201fef6cc9e3c
gams <gustavomaedoyt@gmail.com>
2021-10-19 18:27:12 -0300

    Disease lower case

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
 from flask import Flask, request
-import numpy as np
-import pickle
-import pandas as pd
-from prediction import predictDisease
+from flask_cors import CORS, cross_origin
+
+from prediction import predict_disease
 
 app = Flask(__name__)
 
+cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-#model = pd.read_pickle(r'disease.pkl')
 
-#with open('disease.pickle', 'rb') as f:
-#    model = pickle.load(f)         
-
-        
 @app.route('/', methods=['GET'])
+@cross_origin()
 def home():
     return 'Apollo API<br>Version 1.0.0<br>© 2021'
 
 
 @app.route("/disease", methods=["GET"])
 def pred_disease():
-    
+    prediction = ""
     if request.method == 'GET':
-        
-        symptoms = request.args.get('symptoms') #np.array
+
+        symptoms = request.args.get('symptoms')  # np.array
 
         if symptoms is None:
             return ""
-        
-        prediction = predictDisease(str(symptoms))
-        
+
+        prediction = predict_disease(str(symptoms))
+
     return str(prediction)
-        
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
--- prediction.py
+++ prediction.py
@@ -5,10 +5,10 @@ from sklearn.preprocessing import LabelEncoder
 import pickle
 
 final_rf_model = pickle.load(open('rf_model.pkl', 'rb'))
-final_nb_model = pickle.load(open('nb_model.pkl', 'rb'))
+final_nb_model = pickle.load(open('rf_model.pkl', 'rb'))
 final_svm_model = pickle.load(open('svm_model.pkl', 'rb'))
 
-data = pd.read_csv("./datasets/training.csv", encoding = "ISO-8859-1").dropna(axis=1)
+data = pd.read_csv("./datasets/training.csv", encoding="ISO-8859-1").dropna(axis=1)
 
 encoder = LabelEncoder()
 data["prognosis"] = encoder.fit_transform(data["prognosis"])
@@ -21,9 +21,7 @@ symptoms = X.columns.values
 
 symptom_index = {}
 for index, value in enumerate(symptoms):
-    symptom = " ".join([i.capitalize() for i in value.split("_")])
-    symptom_index[symptom] = index
-    
+    symptom_index[value] = index
 
 data_dict = {
     "symptom_index": symptom_index,
@@ -31,29 +29,30 @@ data_dict = {
 }
 
 
-def predictDisease(symptoms):
+def predict_disease(symptoms):
     symptoms = symptoms.split(",")
-    
-    #criando dados de input
+
+    # criando dados de input
     input_data = [0] * len(data_dict["symptom_index"])
     for symptom in symptoms:
-        index = data_dict["symptom_index"][symptom]
+        x = list(k for k, v in data_dict["symptom_index"].items() if symptom.lower() in k.lower())
+        if len(x) <= 0:
+            x = ['headache']
+        index = data_dict["symptom_index"][x[0]]
         input_data[index] = 1
-        
-    input_data = np.array(input_data).reshape(1,-1)
-    
+
+    input_data = np.array(input_data).reshape(1, -1)
+
     rf_prediction = data_dict["prediction_classes"][final_rf_model.predict(input_data)[0]]
     nb_prediction = data_dict["prediction_classes"][final_nb_model.predict(input_data)[0]]
     svm_prediction = data_dict["prediction_classes"][final_svm_model.predict(input_data)[0]]
 
     final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
+
+    dataDesc = pd.read_csv("./datasets/symptom_description.csv", encoding="ISO-8859-1")
+    #description = dataDesc.loc[dataDesc['Disease'] == final_prediction, 'Description'].iloc[0]
     predictions = {
-        "Previsão do modelo 'Random Forest'": rf_prediction,
-        "Previsão do modelo 'Naive Bayes": nb_prediction,
-        "Previsão do modelo 'SVM'": nb_prediction,
-        "Previsão Final": final_prediction
+        "disease": final_prediction,
+        #"description": description
     }
     return predictions
-
-    
-            
